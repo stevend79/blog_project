72f424ea155a3c5a90f912fb75ca61fa3a71dd40
stevend79 <steven.duchesne1@snhu.edu>
2024-12-15 17:45:44 -0500

    Initial commit - Blog App allows me to post about things like my projects

---
+++ blog/views.py
@@ -0,0 +1,10 @@
+from django.views.generic import ListView, DetailView
+from .models import Post
+
+class BlogListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'
+
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html' 
